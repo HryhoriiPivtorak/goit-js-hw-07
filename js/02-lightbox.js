0299fdb59a20308738ca7dba6bc270faf05924c3
Hryhorii Pivtorak <grigoriypivtorak@gmail.com>
2022-12-28 00:06:51 +0200

    Update_hw_07_part_2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryImg = document.querySelector(".gallery");
+const cardsMarkup = createGalleryCards(galleryItems);
+
+galleryImg.insertAdjacentHTML("beforeend", cardsMarkup);
+
+galleryImg.addEventListener("click", onPalletContainerClick);
+
+function createGalleryCards(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+            <a class="gallery__item" href="${original}">
+              <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+              />
+            </a>
+            `;
+    })
+    .join("");
+}
+
+function onPalletContainerClick(evt) {
+  evt.preventDefault();
+
+  var lightbox = new SimpleLightbox('.gallery a');
+
+//   gallery.next();
+}
 
 console.log(galleryItems);
